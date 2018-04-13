c5be14191b8acfdb94fc426bfbd29042ca2845f2
aman052 <amankumarsrivastava01@gmail.com>
2018-04-13 15:23:54 +0530

    aman-

--- app/src/main/java/com/example/android/whitebear/Models/EventsModel.java
+++ app/src/main/java/com/example/android/whitebear/Models/EventsModel.java
@@ -8,12 +8,20 @@ public class EventsModel {
 
     private String cusId;
     private String eventType;
-    private String venue;
+    private String cusname;
+    private String eventId;
+    private String eventVenue;
+    private String guestCount;
+    private String cuisineType;
 
-    public EventsModel(String cusId, String eventType, String venue) {
+    public EventsModel(String cusId, String cusname,String eventId,String eventType, String eventVenue,String guestCount,String cuisineType) {
         this.cusId = cusId;
         this.eventType = eventType;
-        this.venue = venue;
+        this.eventVenue = eventVenue;
+        this.cusname=cusname;
+        this.eventId=eventId;
+        this.guestCount=guestCount;
+        this.cuisineType=cuisineType;
     }
 
     public String getCusId() {
@@ -24,19 +32,52 @@ public class EventsModel {
         return eventType;
     }
 
-    public String getVenue() {
-        return venue;
+    public String getEventVenue() {
+        return eventVenue;
+    }
+
+    public String getCusname() {
+        return cusname;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public String getGuestCount() {
+        return guestCount;
+    }
+
+    public String getCuisineType() {
+        return cuisineType;
     }
 
     public void setCusId(String cusId) {
         this.cusId = cusId;
     }
 
+    public void setCusname(String cusname) {
+        this.cusname = cusname;
+    }
+
     public void setEventType(String eventType) {
         this.eventType = eventType;
     }
 
-    public void setVenue(String venue) {
-        this.venue = venue;
+    public void setEventVenue(String eventVenue) {
+        this.eventVenue = eventVenue;
     }
+
+    public void setCuisineType(String cuisineType) {
+        this.cuisineType = cuisineType;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public void setGuestCount(String guestCount) {
+        this.guestCount = guestCount;
+    }
+
 }
